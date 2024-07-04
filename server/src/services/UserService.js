af3b8ecb87571655007ae529c26806db389c38c1
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-07-04 12:02:39 +0600

    add

--- server/src/services/UserService.js
+++ server/src/services/UserService.js
@@ -43,7 +43,7 @@ const LoginUserService = async (req, res) => {
         maxAge: process.env.Cookie_Expire_Time,
         httpOnly: true,
         sameSite: 'none',
-        secure: true,
+        secure: false,
       };
 
       // Set cookie
