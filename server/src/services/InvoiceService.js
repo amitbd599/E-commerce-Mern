efe39af2474268f690f3c050e7d8c315def9c36e
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-07-11 13:16:30 +0600

    add update

--- server/src/services/InvoiceService.js
+++ server/src/services/InvoiceService.js
@@ -128,10 +128,10 @@ const CreateInvoiceService = async (req) => {
         store_id: "theme664dfb04bfaf4",
         store_passwd: "theme664dfb04bfaf4@ssl",
         currency: "BDT",
-        success_url: "http://localhost:5000/api/v1/payment-success",
-        fail_url: "http://localhost:5000/api/v1/payment-fail",
-        cancel_url: "http://localhost:5000/api/v1/payment-cancel",
-        ipn_url: "http://localhost:5000/api/v1",
+        success_url: "https://e-commerce-mern-blue.vercel.app/api/v1/payment-success",
+        fail_url: "https://e-commerce-mern-blue.vercel.app/api/v1/payment-fail",
+        cancel_url: "https://e-commerce-mern-blue.vercel.app/api/v1/payment-cancel",
+        ipn_url: "https://e-commerce-mern-blue.vercel.app/api/v1",
         init_url: "https://sandbox.sslcommerz.com/gwprocess/v3/api.php",
       };
 
